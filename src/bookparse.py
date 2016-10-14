9d35c916e3acb89d69d6d3831e8d670f6a05f790
Thomas Harlan <thomasrharlan@gmail.com>
2016-10-14 17:26:05 -0400

    Pylinted code and improved algorithm.

--- src/bookparse.py
+++ src/bookparse.py
@@ -1,3 +1,7 @@
+'''
+Book Parse is an ultra-efficient Python string parser.
+'''
+
 import operator
 import time
 import argparse
@@ -5,65 +9,84 @@ import os
 import sys
 import math
 
-def main(args):
-    results_file = validated_output_file(args.output)
-    result = ''
+CHUNK_SIZE = 1024 #1 Kb
 
-    #Write out to results file
-    for key, value in processed_file():
-        result += key + ': ' + str(value) + '\n'
+def main():
+    '''
+    Main thread execution.
+    '''
+    start_time = time.time()
 
-    results_file.write(result)
-    results_file.close()
+    # Get arguments
+    parser = argparse.ArgumentParser(description='Book Parse: String Parser/Letter Counter')
+    parser.add_argument('-i', '--input', help='Input File Location', required=True)
+    parser.add_argument('-o', '--output', help='Output File Location', required=False)
+    args = parser.parse_args()
 
-def validated_input_file(input):
+    with _validated_output_file(args.output) as results_file:
+        result = ''
+
+        #Write out to results file
+        for key, value in _ordered_dic(args.input):
+            result += '%s: %s\r\n' % (key, str(value))
+
+        results_file.write(result)
+
+    end_time = time.time()
+    print 'Total time: %.2f seconds' % (end_time - start_time)
+
+def _validated_input_file(i_file):
     try:
-        return open(args.input, 'r')
-    except IOError, e:
+        return open(i_file, 'r')
+    except IOError:
         print 'Enter a valid input file'
-        os._exit(1)
+        sys.exit(1)
 
-def validated_output_file(output):
-    if output is not None:
+def _validated_output_file(o_file):
+    if o_file is not None:
         try:
-            return open(args.output, 'w')
-        except IOError, e:
+            return open(o_file, 'w')
+        except IOError:
             print 'Enter a valid output file'
-            os._exit(1)
+            sys.exit(1)
     else:
         #Return new file in this folder
         return open('bookparseResults.txt', 'w') 
 
-def processed_file():
+def read_in_chunks(input_file, chunk_size=CHUNK_SIZE):
+    '''
+    Lazy function (generator) to read the file piece by piece.
+    '''
+    while True:
+        data = input_file.read(chunk_size)
+        if not data:
+            break
+        yield data
+
+def _ordered_dic(i_file):
     #Set up progress bar
     toolbar_length = 40
-    sys.stdout.write('[%s]' % (' ' * toolbar_length))
+    sys.stdout.write('[ %s]' % (' ' * toolbar_length))
     sys.stdout.flush()
-    sys.stdout.write('\b' * (toolbar_length+1))
+    sys.stdout.write('\b' * (toolbar_length + 1))
 
-    with validated_input_file(args.input) as file:
+    with _validated_input_file(i_file) as input_file:
         dic = {}
         inc = 0
 
-        #Determine amount of chunks (for progress bar update)
-        file_size = os.path.getsize(args.input)
-        chunk_size = 1024    #1 Kb - Possibly make this dynamic?
-        total_chunks = file_size / chunk_size
-        total_chunks = math.ceil(total_chunks)
-        increment = total_chunks/toolbar_length
+        #Determine number of chunks (for progress bar update)
+        file_size = os.path.getsize(i_file)
+        total_chunks = math.ceil(file_size / CHUNK_SIZE)
+        increment = total_chunks / toolbar_length
 
         #Iterate through chunks of the file
-        for chunk in iter((lambda:file.read(chunk_size)),''):
-
-            #Strip whitespace; count letters
-            chunk_nws = "".join(chunk.lower().split())
-            txt = filter(lambda x: x.isalpha(), chunk_nws)
-            for c in txt:
-                dic[c] = 1 if not c in dic else dic[c] + 1
+        for chunk in read_in_chunks(input_file):
+            for char in [x for x in chunk.lower() if x.isalpha()]:
+                dic[char] = 1 if not char in dic else dic[char] + 1
 
             #Update progress bar
             inc += 1
-            if(inc >= increment):
+            if inc >= increment:
                 inc = 0
                 sys.stdout.write('-')
                 sys.stdout.flush()
@@ -72,14 +95,4 @@ def processed_file():
     return sorted(dic.items(), key=operator.itemgetter(1), reverse=True)
 
 if __name__ == '__main__':
-    start = time.time()
-
-    parser = argparse.ArgumentParser(description='Book Parse: String Parser/Letter Counter')
-    parser.add_argument('-i','--input', help='Input File Location', required=True)
-    parser.add_argument('-o','--output', help='Output File Location', required=False)
-    args = parser.parse_args()
-
-    main(args)
-
-    end = time.time()
-    print('Total time: %.2f seconds' % (end - start))
+    main()
